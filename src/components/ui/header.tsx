74c8994c2f1ab6b9c88e8d527844c3645fefd265
Joao Fabris <jvfabris21@gmail.com>
2023-10-17 18:20:47 -0300

    chore: add migration

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,14 +1,49 @@
 import React from "react";
-import { Card, CardContent } from "./card";
+import { Card } from "./card";
 import { Button } from "./button";
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  MenuIcon,
+  PercentCircleIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
+import { SheetTrigger, Sheet, SheetContent, SheetHeader } from "./sheet";
 
 const Header = () => {
   return (
     <Card className="flex justify-between p-[1.875rem] items-center">
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side={"left"}>
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+          <div className="mt-2 flex flex-col gap-3">
+            <Button variant={"outline"} className="w-full justify-start gap-2">
+              <LogInIcon size={16} /> Fazer Login{" "}
+            </Button>
+
+            <Button variant={"outline"} className="w-full justify-start gap-2">
+              <HomeIcon size={16} /> Início{" "}
+            </Button>
+
+            <Button variant={"outline"} className="w-full justify-start gap-2">
+              <PercentCircleIcon size={16} /> Ofertas{" "}
+            </Button>
+
+            <Button variant={"outline"} className="w-full justify-start gap-2">
+              <ListOrderedIcon size={16} /> Catálogo{" "}
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
 
       <h1 className="text-lg font-semibold">
         <span className="text-primary">e-commerce </span> Store{" "}
