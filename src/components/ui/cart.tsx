60c41ca9b12c2238efea88fc9b73d13f87fcf9ed
Joao Fabris <jvfabris21@gmail.com>
2023-11-03 17:19:11 -0300

    feat: products on cart in progress...

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -2,11 +2,13 @@ import { ShoppingCartIcon } from "lucide-react";
 import { Badge } from "./badge";
 import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
+import CartItem from "./cart-item";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 const Cart = () => {
   const { products } = useContext(CartContext);
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base up"
         variant="outline"
@@ -14,9 +16,14 @@ const Cart = () => {
         <ShoppingCartIcon size={16} />
         Catálogo
       </Badge>
-      {products.map((product) => (
-        <h1 key={product.id}>{product.name}</h1>
-      ))}
+      <div className="flex flex-col gap-5">
+        {products.map((product) => (
+          <CartItem
+            key={product.id}
+            product={computeProductTotalPrice(product as any) as any}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { Product } from "@prisma/client";
+import { ProductWithTotalPrice } from "@/helpers/product";
 import { ReactNode, createContext, useState } from "react";
 
-interface CartProduct extends Product {
+export interface CartProduct extends ProductWithTotalPrice {
   quantity: number;
 }
 
@@ -12,7 +12,7 @@ interface IcartContext {
   cartTotalPrice: number;
   cartBasePrice: number;
   cartTotalDiscount: number;
-  addProductToCart: (product: CartProduct) => void// n retorna valor, apenas uma ação;
+  addProductToCart: (product: CartProduct) => void; // n retorna valor, apenas uma ação;
 }
 
 export const CartContext = createContext<IcartContext>({
@@ -27,6 +27,28 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
   const [products, setProducts] = useState<CartProduct[]>([]);
 
   const addProductToCart = (product: CartProduct) => {
+    // se o produto ja estiver no carrinho, apenas aumente a quantidade
+
+    const productAlreadyOnCart = products.some(
+      (cartProduct) => cartProduct.id === product.id
+    );
+
+    if (productAlreadyOnCart) {
+      setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + product.quantity,
+            };
+          }
+          return cartProduct;
+        })
+      );
+      return;
+    }
+
+    // se n, adicione o produto a lista
     setProducts((prev) => [...prev, product]);
   };
 
