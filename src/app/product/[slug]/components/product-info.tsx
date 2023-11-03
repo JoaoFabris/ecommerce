302910adb1b638b54d2aa63b0527eebe1b9202c2
Joao Fabris <jvfabris21@gmail.com>
2023-11-03 15:37:38 -0300

    feat: ensure product can be add to cart

--- src/app/product/[slug]/components/product-info.tsx
+++ src/app/product/[slug]/components/product-info.tsx
@@ -3,26 +3,22 @@
 import { Button } from "@/components/ui/button";
 import DiscountBadge from "@/components/ui/discount-badge";
 import { ProductWithTotalPrice } from "@/helpers/product";
-import {
-  ArrowDownIcon,
-  ArrowLeftIcon,
-  ArrowRight,
-  ArrowRightIcon,
-  TruckIcon,
-} from "lucide-react";
-import { useState } from "react";
+import { CartContext } from "@/providers/cart";
+import { ArrowLeftIcon, ArrowRightIcon, TruckIcon } from "lucide-react";
+import { useContext, useState } from "react";
 
 interface ProductInfoProps {
-  product: Pick<
-    ProductWithTotalPrice,
-    "basePrice" | "description" | "discountPercentage" | "totalPrice" | "name"
-  >;
+  product: ProductWithTotalPrice;
 }
 
-const ProductInfo = ({
-  product: { basePrice, description, discountPercentage, totalPrice, name },
-}: ProductInfoProps) => {
+const ProductInfo = ({ product }: ProductInfoProps) => {
   const [quantity, setQuantity] = useState(1);
+
+  const { addProductToCart } = useContext(CartContext);
+
+  const handleAddToCartClick = () => {
+    addProductToCart({ ...product, quantity});
+  };
 
   const handleDecreaseQuantityClick = () => {
     setQuantity((prev) => (prev === 1 ? prev : prev - 1));
@@ -34,20 +30,20 @@ const ProductInfo = ({
 
   return (
     <div className="flex flex-col px-5">
-      <h2 className="text-lg">{name}</h2>
+      <h2 className="text-lg">{product.name}</h2>
 
       <div className="flex items-center gap-2">
-        <h1 className="text-xl font-bold">R$ {totalPrice.toFixed(2)}</h1>
-        {discountPercentage > 0 && (
-          <DiscountBadge>
-            {discountPercentage}
-          </DiscountBadge>
+        <h1 className="text-xl font-bold">
+          R$ {product.totalPrice.toFixed(2)}
+        </h1>
+        {product.discountPercentage > 0 && (
+          <DiscountBadge>{product.discountPercentage}</DiscountBadge>
         )}
       </div>
 
-      {discountPercentage > 0 && (
+      {product.discountPercentage > 0 && (
         <p className="text-sm line-through opacity-75">
-          De: R$ {Number(basePrice).toFixed(2)}
+          De: R$ {Number(product.basePrice).toFixed(2)}
         </p>
       )}
       <div className="flex items-center gap-2 mt-4">
@@ -72,10 +68,12 @@ const ProductInfo = ({
 
       <div className="flex flex-col gap-3 mt-8">
         <h3 className="font-bold">Descrição</h3>
-        <p className="text-sm opacity-60 text-justify">{description}</p>
+        <p className="text-sm opacity-60 text-justify">{product.description}</p>
       </div>
 
-      <Button className="mt-8 font-bold uppercase">
+      <Button className="mt-8 font-bold uppercase"
+      onClick={handleAddToCartClick}
+      >
         {" "}
         Adicionar ao carrinho{" "}
       </Button>
---
+++ src/providers/cart.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Product } from "@prisma/client";
+import { ReactNode, createContext, useState } from "react";
+
+interface CartProduct extends Product {
+  quantity: number;
+}
+
+interface IcartContext {
+  products: CartProduct[];
+  cartTotalPrice: number;
+  cartBasePrice: number;
+  cartTotalDiscount: number;
+  addProductToCart: (product: CartProduct) => void// n retorna valor, apenas uma ação;
+}
+
+export const CartContext = createContext<IcartContext>({
+  products: [],
+  cartTotalPrice: 0,
+  cartBasePrice: 0,
+  cartTotalDiscount: 0,
+  addProductToCart: () => {},
+});
+
+const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
+
+  const addProductToCart = (product: CartProduct) => {
+    setProducts((prev) => [...prev, product]);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        products,
+        addProductToCart,
+        cartTotalPrice: 0,
+        cartBasePrice: 0,
+        cartTotalDiscount: 0,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
